88733994f85a9e6ad95eca7d7cda5aa3bdbfd231
10151688 <yurim2220@gmail.com>
2023-10-06 23:55:46 +0900

    initial commit

---
+++ src/test/java/com/study/QueryDsl_Project/QueryDslProjectApplicationTests.java
@@ -0,0 +1,37 @@
+package com.study.QueryDsl_Project;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.QueryDsl_Project.entity.Hello;
+import com.study.QueryDsl_Project.entity.QHello;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QueryDslProjectApplicationTests {
+
+	@Autowired
+	EntityManager em;
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+	}
+
+}
+

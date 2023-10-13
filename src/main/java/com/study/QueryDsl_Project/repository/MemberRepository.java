52ba6271da30a42c342e2818bdb1415660f00d91
10151688 <yurim2220@gmail.com>
2023-10-13 16:23:50 +0900

    QuerydslPredicateExecutor

--- src/main/java/com/study/QueryDsl_Project/repository/MemberRepository.java
+++ src/main/java/com/study/QueryDsl_Project/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package com.study.QueryDsl_Project.repository;
 
 import com.study.QueryDsl_Project.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member,Long> , MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member,Long> , MemberRepositoryCustom , QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 
--- src/test/java/com/study/QueryDsl_Project/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/QueryDsl_Project/repository/MemberRepositoryTest.java
@@ -3,17 +3,20 @@ package com.study.QueryDsl_Project.repository;
 import com.study.QueryDsl_Project.dto.MemberSearchCondition;
 import com.study.QueryDsl_Project.dto.MemberTeamDto;
 import com.study.QueryDsl_Project.entity.Member;
+import com.study.QueryDsl_Project.entity.QMember;
 import com.study.QueryDsl_Project.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static com.study.QueryDsl_Project.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -45,21 +48,7 @@ class MemberRepositoryTest {
 
     @Test
     public void searchTest() {
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
-
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
-
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
+        prepareData();
 
         MemberSearchCondition condition = new MemberSearchCondition();
         condition.setAgeGoe(35);
@@ -76,6 +65,18 @@ class MemberRepositoryTest {
 
     @Test
     public void searchPageSimple() {
+        prepareData();
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0,3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition , pageRequest);
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+    }
+
+    private void prepareData() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -91,14 +92,24 @@ class MemberRepositoryTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+    }
 
-        MemberSearchCondition condition = new MemberSearchCondition();
-        PageRequest pageRequest = PageRequest.of(0,3);
 
-        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition , pageRequest);
+    @Test
+    public void querydslPredicateExecutorTest() {
 
-        assertThat(result.getSize()).isEqualTo(3);
-        assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+        prepareData();
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member3")));
+        for (Member member1 : result) {
+            System.out.println("member = " + member1);
+        }
+
+        // QuerydslPredicateExecutor<Member> 단점
+        // 1. join 불가능
+        // 2. 서비스 계층에서 QueryDsl 에 의존하게 됨
+        // 3. 복잡한 실무환경에서 사용하기에는 한계가 명확
+        // cf. Pageable , Sort 지원
     }
 
 }
